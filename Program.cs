bac7f0f30ff8306a0c7c53f8cc7a0244431defd2
F00XY <nathaliahelenna@gmail.com>
2024-01-06 12:22:49 -0300

    concluido!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
+Console.WriteLine("Smartphone Nokia");
+Nokia c1= new Nokia("123456", "Modelo teste", "ImeiTeste",64);
+c1.Ligar();
+// c1.Modelo();--> privado então nao tem como exibir
+// c1.Imei(); --> privado então nao tem como exibir
+// c1.Memoria();--> privado então nao tem como exibir
+c1.ReceberLigacao();
+c1.InstalarAplicativo("App1");
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("\nSmartphone Iphone");
+Nokia c2= new Nokia("654321", "Modelo teste2", "ImeiTeste2",128);
+c2.Ligar();
+// c1.Modelo();--> privado então nao tem como exibir
+// c1.Imei(); --> privado então nao tem como exibir
+// c1.Memoria();--> privado então nao tem como exibir
+c2.ReceberLigacao();
+c2.InstalarAplicativo("App2");
