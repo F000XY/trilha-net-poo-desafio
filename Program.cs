1bd2a4c9551f54e7f30359a129c26b80b7c66951
Nathália Helenna <148366887+F000XY@users.noreply.github.com>
2024-01-06 21:24:14 -0300

    Atualizar o Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using DesafioPOO.Models;
+using DesafioPOO.Models;
 Console.WriteLine("Smartphone Nokia");
 Nokia c1= new Nokia("123456", "Modelo teste", "ImeiTeste",64);
 c1.Ligar();
@@ -9,7 +9,7 @@ c1.ReceberLigacao();
 c1.InstalarAplicativo("App1");
 
 Console.WriteLine("\nSmartphone Iphone");
-Nokia c2= new Nokia("654321", "Modelo teste2", "ImeiTeste2",128);
+Iphone c2= new Iphone ("654321", "Modelo teste2", "ImeiTeste2",128);
 c2.Ligar();
 // c1.Modelo();--> privado então nao tem como exibir
 // c1.Imei(); --> privado então nao tem como exibir
